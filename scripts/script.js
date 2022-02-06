c78d4a4d9c2d19ab048deb575187330cc83e55f9
AguRivarola <agurivarola@gmail.com>
2022-02-06 14:31:07 -0300

    Cambio algo de estructura, modifico visual y realizo media query responsive

--- scripts/script.js
+++ scripts/script.js
@@ -48,7 +48,7 @@ window.addEventListener('load', function () {
     const ctx = canvas.getContext('2d');
 
     const w = canvas.width = document.body.clientWidth;
-    const h = canvas.height; //= document.body.clientHeight;
+    const h = canvas.height //= document.body.clientHeight;
     const cols = Math.floor(w / 20) + 1;
     const ypos = Array(cols).fill(0);
 
